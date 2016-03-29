bd63005d25c873d97271447588b5e5564222871d
Heikki Moisio <heikki.moisio@visma.com>
2016-03-29 14:39:13 +0300

    Added content-type validation.

--- src/test/java/com/mulesoft/training/HelloMavenTest.java
+++ src/test/java/com/mulesoft/training/HelloMavenTest.java
@@ -16,12 +16,12 @@ public class HelloMavenTest extends FunctionalTestCase {
 
     // retrieveFlights response contains content-type header
     // content-type headers has a value of application/json
-    @Test
-   /* public void retrieveFlightsAddsAppropriateHeader() throws Exception{
+   @Test
+    public void retrieveFlightsAddsAppropriateHeader() throws Exception{
       MuleEvent event = runFlow("retrieveFlights");
       String contentType = event.getMessage().getOutboundProperty("Content-Type");
       assertEquals("application/json", contentType);
-    }*/
+    }
 
     // run maven-config.xml when testing
     @Override
